9df28c57a358a1c4a8b599675f196d8a5a979be8
javaugi <javaugi@yahoo.com>
2025-08-02 12:03:34 -0400

    removed some secrets

--- cdvite.cmd
+++ cdvite.cmd
@@ -1,3 +1,3 @@
-set PROJ_HOME=E:/springtutorial/cimathformulas
+set PROJ_HOME=C:/Users/javau/dev/projects/sb-j2ee/sboot-rest-mformula-orig
 REM "go to reactivite proj from $PROJ_HOME "
 cd %PROJ_HOME%/src/main/webapp/reactivite
